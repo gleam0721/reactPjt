14f6a2560dc1e3270258d74fc8b8cb3c191d2f9a
박은영 <gleam@naver.com>
2023-10-20 10:32:34 +0900

    blog start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let post = '선유도 맛집 리스트';
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={ {color : 'red', fontSize : '16px'} }>블로그</h4>
+      </div>
+      <h4>{ post }</h4>
+    </div>
+  );
+}
+
+export default App;
